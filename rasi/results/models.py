459cf5202fdb94589075f62c4aaf2f3be22a6ed8
Franisis <f.guzmana@uniandes.edu.co>
2023-11-11 14:50:13 -0500

    napoleon

--- rasi/results/models.py
+++ rasi/results/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Result(models.Model):
-    result = models.CharField(max_length=50)    
+    result = models.CharField(max_length=50, null = True)    
     datetime = models.DateTimeField(auto_now_add=True)
-    meaurement = models.CharField(max_length=50)
-    apreciation = models.CharField(max_length=50)
+    meaurement = models.CharField(max_length=50, null = True)
+    apreciation = models.CharField(max_length=50, null = True)
 
 def __str__(self):
     return '%s %s' % (self.result, self.apreciation)
