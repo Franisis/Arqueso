6106294059a87499c8456083b920519d4db3226c
Franisis <f.guzmana@uniandes.edu.co>
2023-11-18 12:29:56 -0500

    me mame

--- rasi/results/views.py
+++ rasi/results/views.py
@@ -17,16 +17,13 @@ from rasi.auth0backend import Auth0
 
 
 # Create your views here.
-@login_required
 def resultGet(request):
-        r = Auth0.getRole(request)
-        if r == "admin" or r =="medic":
-            print(request)
-            if request.method=="GET":
-                results = rl.get_results()
-                results_dto = serializers.serialize('json', results)
-                return HttpResponse(results_dto, "application/json" )
-    
+    print(request)
+    if request.method=="GET":
+        results = rl.get_results()
+        results_dto = serializers.serialize('json', results)
+        return HttpResponse(results_dto, "application/json" )
+
     
 @login_required
 def resultPost(request):
