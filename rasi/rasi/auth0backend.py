33ac02e7faa4869b92a28fc7baf337942a05e04d
Franisis <f.guzmana@uniandes.edu.co>
2023-11-20 13:18:49 -0500

    mew

--- rasi/rasi/auth0backend.py
+++ rasi/rasi/auth0backend.py
@@ -40,7 +40,7 @@ def getRole(request):
     url = "https://isis2503-sprints.us.auth0.com"
     headers = {'authorization': 'Bearer ' + accessToken}
     resp = requests.get(url, headers=headers)
-    print(json.loads(resp.text), "T-T")
+    print(json.loads(resp.content), "T-T")
     userinfo = resp.json()
     role = userinfo['isis2503-sprints.us.auth0.com']
     return (role)
